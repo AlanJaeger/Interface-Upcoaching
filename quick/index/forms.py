9c327b3082f328649be1c274821f57b1837c7eb5
Diego <diego_shakan@hotmail.com>
2019-01-24 16:01:17 -0300

    add login/logout app

--- quick/index/forms.py
+++ quick/index/forms.py
@@ -2,7 +2,26 @@ from django.forms import ModelForm
 from .models import ProfessoresInteressados, Aulas
 from django.forms import Form, ModelForm, TextInput, DateField, DateInput, ImageField, FileInput, CharField, IntegerField,Select, TimeInput
 from django.utils.timezone import now
+from django import forms
+from django.contrib.auth.models import User
 
+# Formulário de Cadastro de Usuário
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email']
+        widgets ={
+            'username': forms.TextInput(attrs={'max_length': 100}),
+            'password': forms.PasswordInput(attrs={'max_length': 100}),
+            'email': forms.EmailInput(attrs={'max_length': 100}),
+        }
+
+    def save(self, commit=True):
+        user = super(UserForm, self).save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
 
 class InteresseForm(ModelForm):
     class Meta:
